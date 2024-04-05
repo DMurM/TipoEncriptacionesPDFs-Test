2982d1b8ae56bc866b3a726db260d095b175856a
Oscar Mur <84236800+DMurM@users.noreply.github.com>
2024-04-05 19:53:53 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 Repo para web de desencriptacion de documentos PDF con formato de contraseña Adobe Acrobat
+
+--Remember
+    -Whenever you feel like giving up, think of all the people that would love
+    to see you fail.
+
+--Instalación en Ubuntu Test
+
+    -mkdir -p ~/src
+    -sudo apt-get -y install git build-essential libssl-dev zlib1g-dev
+
+    -recomended 
+    -sudo apt-get -y install yasm pkg-config libgmp-dev libpcap-dev libbz2-dev
+
+    -For Nvidia
+    -sudo apt-get -y install nvidia-opencl-dev
+
+    -For Amd
+    -sudo apt-get -y install ocl-icd-opencl-dev opencl-headers
+
+    -Inici instalació Repo
+    -cd ~/src
+    -git clone https://github.com/openwall/john -b bleeding-jumbo john
+
+    -Build
+    -cd ~/src/john/src
+    -./configure && make -s clean && make -sj4
+
+    -Test build
+    -cd ~/src/john/run
+    -./john --test=0
+
+    -Benchmark
+    -./john --test
+    
+--Instalacion Windows
+
+    -Ruta cd /cygdrive/c/Users/Daniel/Desktop/jhonjumbo/john-bleeding-jumbo/src
+
+    -./configure && make -s clean && make -sj4
+    -make windows-package
--- WebEncryptingtest/app.py
+++ WebEncryptingtest/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_cors import CORS
 import os
+import subprocess
+import csv
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +12,54 @@ ALLOWED_EXTENSIONS = {'pdf'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def find_password_in_csv(pdf_hash):
+    csv_file = 'passwords.csv'
+    with open(csv_file, newline='') as file:
+        reader = csv.reader(file)
+        next(reader) 
+        for row in reader:
+            if row[1] == pdf_hash:
+                return row[0]  # Retorna la contraseña si el hash coincide
+    return None
+
+def save_hash_to_csv(pdf_filename, pdf_hash):
+    csv_file = 'passwords.csv'  # Nombre del archivo CSV para almacenar contraseñas y hashes
+    if not is_hash_in_csv(pdf_hash):  # Verifica si el hash ya existe en el CSV
+        with open(csv_file, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([pdf_filename, pdf_hash])
+
+def is_hash_in_csv(pdf_hash):
+    csv_file = 'passwords.csv'
+    with open(csv_file, newline='') as file:
+        reader = csv.reader(file)
+        next(reader) 
+        for row in reader:
+            if row[1] == pdf_hash:
+                return True
+    return False
+
+def convert_pdf_to_hash(pdf_path):
+    try:
+        perl_exe_path = r'C:\Strawberry\perl\bin\perl.exe'  # Ruta al intérprete de Perl
+        pdf2john_script = r'C:\john\run\pdf2john.pl'        # Ruta al script pdf2john.pl
+
+        result = subprocess.run([perl_exe_path, pdf2john_script, pdf_path], capture_output=True, text=True)
+
+        if result.returncode == 0:
+            pdf_hash = result.stdout.strip()
+            save_hash_to_csv(os.path.basename(pdf_path), pdf_hash)  # Guarda el hash en el CSV si no existe
+            password = find_password_in_csv(pdf_hash)
+            if password:
+                return f"Contraseña: {password}\nHash: {pdf_hash}"
+            else:
+                return f"No se encontró la contraseña para el hash: {pdf_hash}"
+        else:
+            return None
+    except Exception as e:
+        print(f"Error al convertir el archivo PDF a hash: {e}")
+        return None
+
 @app.route('/upload_pdf', methods=['POST'])
 def upload_pdf():
     if 'pdf' not in request.files:
@@ -18,12 +68,19 @@ def upload_pdf():
     if file.filename == '':
         return 'Ningún archivo seleccionado.'
     if file and allowed_file(file.filename):
-        if not os.path.exists('WebEncryptingtest/pdf_files'): #Substituir los 3 directorios para enviar los pdfs en "x" directorio
-            os.makedirs('WebEncryptingtest/pdf_files') #
-        file.save(os.path.join('WebEncryptingtest/pdf_files', file.filename)) #
-        return  ('Archivo PDF subido exitosamente. ' + file.filename)
+        if not os.path.exists('pdf_files'):
+            os.makedirs('pdf_files')
+        file_path = os.path.join('pdf_files', file.filename)
+        file.save(file_path)
+
+        hash_result = convert_pdf_to_hash(file_path)
+
+        if hash_result:
+            return hash_result
+        else:
+            return 'El archivo PDF no se pudo convertir.'
     else:
         return 'La extensión del archivo no está permitida o el archivo está vacío.'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5500)
+    app.run(debug=False, port=5500)
